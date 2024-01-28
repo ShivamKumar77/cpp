3e9d83d93e559c3fda4691ad38ff05fc091f3646
shivam <shivamraj8825101374@gmail.com>
2024-01-28 16:21:11 +0530

    first

---
+++ asd.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int r;
+   cin>>r;
+   cout<<"Enter the radius of a circle: ";
+
+   cout<<"The area of a circle is: "<< 3.14*r*r;
+   return 0;
+}

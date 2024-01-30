c23a6aa8560209d9bd5521494be4022ed25b2d28
shivam <shivamraj8825101374@gmail.com>
2024-01-30 18:09:14 +0530

    bubble sort

---
+++ bubblesort.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<conio.h>
+int main()
+{
+    int n;
+    std::cout<<"Enter the number of elements: ";
+    std::cin>>n;
+    int arr[n];
+    std::cout<<"Enter the elements: ";
+    for(int i=0;i<n;i++)
+    {
+        std::cin>>arr[i];
+    }
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=0;j<n-i-1;j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+    std::cout<<"\nElements in sorted order are : \n";
+    for(int i=0;i<n;i++)
+    {
+        std::cout<<arr[i]<<"\t";
+    }
+    return 0;
+}
+/*
+Time Complexity: O(n^2)
+Space Complexity: O(1)
+*/

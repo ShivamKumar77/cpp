6334930cf985a59142067d1376ccf1b83f06e6ac
shivam <shivamraj8825101374@gmail.com>
2024-01-30 17:49:26 +0530

    swapping number

---
+++ swappingnumber.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<conio.h>
+int main()
+{
+    int a,b,c;
+    cout<<"Enter the first number:";
+    cin>>a;
+    cout<<"Enter the second number:";
+    cin>>b;
+    c=a;
+    a=b;
+    b=c;
+    cout<<"The first number is:"<<a;
+    cout<<"The second number is:"<<b;
+    return 0;
+}

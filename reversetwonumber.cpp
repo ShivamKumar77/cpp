2ebe37f279be45f6c43c511ef78bac4c5f36163c
shivam <shivamraj8825101374@gmail.com>
2024-01-30 17:45:43 +0530

    Reverse two number

---
+++ reversetwonumber.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"Enter the first number:";
+    cin>>a;
+    cout<<"Enter the second number:";
+    cin>>b;
+    c=a;
+    a=b;
+    b=c;
+    cout<<"The first number is:"<<a;
+    cout<<"The second number is:"<<b;
+    return 0;
+}

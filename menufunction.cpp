a1e2d9681f23d9ee8626139ee426a2e617248fd5
shivam <shivamraj8825101374@gmail.com>
2024-01-31 21:48:04 +0530

    Age calculator

---
+++ menufunction.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include<conio.h>
+int main()
+{
+    std::cout<<"Hello World";
+    return 0;
+}
+//This program is a simple Hello World program in C++ which prints "Hello World" on the screen. The code includes necessary header files to enable input and output operations using <iostream>. The main function
